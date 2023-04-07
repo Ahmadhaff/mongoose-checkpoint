a47f21af656a2ad3f39c2ca98ec46a16720e1468
Ahmadhaff <haffoudhiahmad219@gmail.com>
2023-04-07 02:08:05 +0100

    Fisrt Commit

--- main.js
+++ main.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 const Person = require("./models/person");
 const express = require("express");
 const app = express();
+require('dotenv').config();
 
 app.use(express.json());
 
-mongoose.connect(
-    "mongodb+srv://haffoudhi:haffoudhi@cluster0.3odwltl.mongodb.net/mongooseCheckPoint?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("Database connected successfully"))
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
 
   app.use('/api' , require('./routes/PersonRoutes'));
 
